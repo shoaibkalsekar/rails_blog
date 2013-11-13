5f242639c8f17b7956ad4ab0b1df6d1324ff32d7
shoaibkalsekar <shoaibkalsekar@gmail.com>
2013-11-13 20:09:44 +0530

    Comments Working..Finally!!

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111192415) do
+ActiveRecord::Schema.define(version: 20131113134708) do
 
   create_table "commenters", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
     t.string   "commenter"
     t.text     "body"
     t.integer  "post_id"
